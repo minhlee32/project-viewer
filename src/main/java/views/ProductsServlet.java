51b3d18b403ef200375dd786dde3d152e76b2cbe
minhlee <minhlee.fre.mta@gmail.com>
2018-08-09 11:54:21 +0700

    add Servlet ProductServlet in package view

---
+++ src/main/java/views/ProductsServlet.java
@@ -0,0 +1,44 @@
+package views;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Product;
+import service.ProductService;
+import service.ProductServiceImpl;
+
+@WebServlet(name = "ProductsServlet", urlPatterns = "/products")
+public class ProductsServlet extends HttpServlet {
+
+    private ProductService productService = new ProductServiceImpl();
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            List<Product> products = productService.findAll();
+            request.setAttribute("products", products);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("products.jsp");
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
